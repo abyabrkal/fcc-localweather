961f3bff347b206336a4c9ce647010f6cd2d32fc
ABY ABRAHAM KALATHIVILA <abyabrkal@gmail.com>
2017-03-29 15:50:20 +0400

    WeatherData worked FIne + Finetuning
    Weatherdata from OpenWeatherMaps as well as location via ip-info worked
    well.
    JS Code beautification
    Added weather icons and main icon displays

--- script.js
+++ script.js
@@ -1,141 +1,156 @@
-/*document.addEventListener('DOMContentLoaded', function() {
-    alert("Ready!");
-}, false);*/
+    /*document.addEventListener('DOMContentLoaded', function() {
+        alert("Ready!");
+    }, false);*/
 
 
-//Global variables
-var cTemp = "";
-var cityIP = "";
-var country = "";
-var countryISO2 = "";
-var dayType = "";
-var winSpd = "";
-var winDir = "";
-var humid = "";
-var sunRiseTime = "";
-var sunSetTime = "";
-var loc = ["Dubai", "AE"]
-
-
-//URL variables
-var baseURL = "http://api.openweathermap.org/data/2.5/weather?q=";
-var unitMTR = "&units=metric";
-var callBACK = "&callback=?";
-var apID = "&appid=49feecc2cd382186ed0d59d785a42457";
-var finalURL = "";
+    //Global variables
+    var cTemp = "";
+    var city = "";
+    var country = "";
+    var countryISO2 = "";
+    var dayType = "";
+    var winSpd = "";
+    var winDir = "";
+    var humid = "";
+    var sunRiseTime = "";
+    var sunSetTime = "";
+    var meridian = "am";
 
 
 
 
 
-// Get City and Country data from IP address
-function getLocation() {
-
-    $.getJSON('http://ipinfo.io/geo', function(response){
-      console.log(response.city, response)
-
-      cityIP = response.city;
-
-      loc[0] = response.city;
-      console.log(loc[0]);
-    });
-
-		//console.log("CITY  INSIDE - " + response.city);
-		console.log("CITY  OUTSIDE - " + loc[0]);
-}
 
 
-/*---------- GET READY ------------------*/
+    // Get City and Country data from IP address
+    function getLocation() {
 
-$(document).ready(function(){
+        $.getJSON('http://ipinfo.io/geo', function(response){
+          console.log(response.city, response)
 
-	  // Get weather data from OpenWeatherMap: https://openweathermap.org
-    getLocation();
+          city = response.city;
+          $('#city').text(response.city);
 
-		console.log("city in gtWthr() before finalURL - " + cityIP);
-	  finalURL = baseURL + 'Dubai' + unitMTR + apID + callBACK;
-		console.log("finalURL - " + finalURL);
+          getWeather(response.city);
 
-		$.ajax({
-		 url: finalURL,
-		 data: {
-				format: 'json'
-		 },
-		 error: function(error) {
-				//$('#info').html('<p>An error has occurred</p>');
-				console.log("ERROR - " + error.message);
-		 },
-		 dataType: 'jsonp',
-		 success: function(data) {
-				console.log("SUCCESS");
-				console.log(data);
+        });
 
-				// load returned weather data to respective variables
-				loadWData(data);
+    		//console.log("CITY  INSIDE - " + response.city);
+    	console.log("CITY  OUTSIDE - " + city);
+    }
 
-		 },
-		 type: 'GET'
-		});
+
+
+    function getTime(unix_timestamp) {
+
+        // Create a new JavaScript Date object based on the timestamp
+        // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+        var date = new Date(unix_timestamp * 1000);
+
+        // Hours part from the timestamp
+        var hours = date.getHours();
+        meridian = " am";
+
+
+        if (hours > 12) {
+            hours = hours - 12;
+            meridian = " pm";
+        }
+
+        // Minutes part from the timestamp
+        var minutes = "0" + date.getMinutes();
+
+        // Seconds part from the timestamp
+        var seconds = "0" + date.getSeconds();
+
+        // Will display time in 10:30  format
+        var formattedTime = hours + ':' + minutes.substr(-2);
+
+        return formattedTime;
+     }
+
+
+
+    // Get weather data from OpenWeatherMap: https://openweathermap.org
+    function getWeather(myCity){
+
+        //URL variables
+        var baseURL = "http://api.openweathermap.org/data/2.5/weather?q=";
+        var unitMTR = "&units=metric";
+        var callBACK = "&callback=?";
+        var apID = "&appid=49feecc2cd382186ed0d59d785a42457";
+
+        var finalURL = baseURL + myCity + unitMTR + apID + callBACK;
+
+
+        $.ajax({
+             url: finalURL,
+             data: {
+                    format: 'json'
+             },
+             error: function(error) {
+                    //$('#info').html('<p>An error has occurred</p>');
+                    console.log("ERROR - " + error.message);
+             },
+             dataType: 'jsonp',
+             success: function(data) {
+                    console.log("SUCCESS");
+                    console.log(data);
+
+                    // load returned weather data to respective variables
+                    parseWeather(data);
+
+             },
+             type: 'GET'
+        });
+
+    }
+
 
 	// Load weather data for display
-	function loadWData (data) {
+	function parseWeather(data) {
 
-		cTemp = data.main.temp;
-		console.log("Temp - " + cTemp);
+		cTemp = Math.round(data.main.temp);
 		$('#temp').text(cTemp);
-
-
-		//getLocation();
-		$('#city').text(cityIP + ', ' + countryISO2);
 
 		dayType = data.weather[0].main;
 		$('#dayType').text(data.weather[0].main);
 
+        //ICON
+        var icon = ("<img class=\"icn\" src='http://openweathermap.org/img/w/" + data.weather[0].icon + ".png'>");
+        $('#icon').html(icon);
+
 		winSpd = data.wind.speed;
-		winDir = data.wind.deg;
+		//winDir = data.wind.deg;
 		$('#windSpeed').text(data.wind.speed + ' m/s');
 		//$('#windDirection').add(data.wind.deg);
 
 		humid = data.main.humidity;
 		$('#humidity').text(data.main.humidity);
 
-
 		sunRiseTime = getTime(data.sys.sunrise);
+        $('#sunrise').text(sunRiseTime + meridian);
 		sunSetTime = getTime(data.sys.sunset);
-		$('#sunrise').text(sunRiseTime + ' AM');
-		$('#sunset').text(sunSetTime + ' PM');
+		$('#sunset').text(sunSetTime + meridian);
 	}
 
-
-
-	function getTime(unix_timestamp) {
-
-  		// Create a new JavaScript Date object based on the timestamp
-  		// multiplied by 1000 so that the argument is in milliseconds, not seconds.
-  		var date = new Date(unix_timestamp*1000);
-
-  		// Hours part from the timestamp
-  		var hours = date.getHours();
-
-  		// Minutes part from the timestamp
-  		var minutes = "0" + date.getMinutes();
-
-  		// Seconds part from the timestamp
-  		var seconds = "0" + date.getSeconds();
-
-  		// Will display time in 10:30  format
-  		var formattedTime = hours + ':' + minutes.substr(-2);
-
-  		return formattedTime;
-	 }
+    function changeTemp(){
+        var fH = ((cTemp * 9)/5) + 32;
+        document.getElementById("temp").innerHTML = fH;
+        document.getElementById("metric").innerHTML = F;
+    }
 
 
 
-		getWData();
+    /*---------- GET READY ------------------*/
+
+    $(document).ready(function(){
+
+        getLocation();
+
+    });
 
 
-
-});
 
 
 
