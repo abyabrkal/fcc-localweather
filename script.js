0a1ffa6dba8ccea172ea6fb0e776c3dfe237fc1a
ABY ABRAHAM KALATHIVILA <abyabrkal@gmail.com>
2018-02-26 17:07:09 +0400

    JS updates

--- script.js
+++ script.js
@@ -20,65 +20,48 @@ var dateL ="";
 // Get Date and Greeting
 function whatTime() {
 	
-  // Get Today's Date for Nav display
-  var date = new Date();
-	
-	document.getElementById('today').innerHTML = date.toDateString();
-	
+	// Get Today's Date for Nav display
+  	var date = new Date();
+  	document.getElementById('today').innerHTML = date.toDateString();
 	
 	// Lets get Greeting
 	var greets = "Good Morning";
-	
-  // Hours part from the timestamp
-  var hours = date.getHours();
-	
-	
+	// Hours part from the timestamp
+  	var hours = date.getHours();
 	if (hours > 12 && hours < 17) {
-    greets = "Good Afternoon";
-  }
+    	greets = "Good Afternoon";
+  	}
 	else if (hours >= 17 && hours < 24){
 		greets = "Good Evening";
 	}
 	else {
 		greets = "Good Morning";
 	}
-
-	
+	// load greeting
 	document.getElementById("greeting").innerHTML = greets;
-  	
 }
 
 function parseWeather(weatherDb){
-	
-		
 	// LOCATION DATA with COUNTRY
 	var location = weatherDb.city + ', ' + weatherDb.country;
 	document.getElementById('country').innerHTML = location;
-	
-	
 	// WEATHER CONDITION
 	document.getElementById('conditionText').innerHTML = weatherDb.conditionText;
-	
 	//ICON SETUP
 	var icon = ("<img class=\"icon\" src='" + weatherDb.icon + "'>");
-  document.getElementById('icon').innerHTML = icon;
-	
+  	document.getElementById('icon').innerHTML = icon;
 	//TEMPERATURE VALUE - CELSIUS
 	document.getElementById('tempC').innerHTML = weatherDb.tempC;
-	
 	//TEMPERATURE UNIT - FAHRENHEIT
 	document.getElementById('tempF').innerHTML = weatherDb.tempF;
-
 }
 
 
 
 /* GET WEATHER from API without jQuery */ 
 function getWeather(city) {
-	
-	
+
 	var finalURL = "https://api.apixu.com/v1/current.json?key=7db709020c694a158aa64848170206&q=" + city;
-	
 	
 	var request = new XMLHttpRequest();
 	request.open('GET', finalURL, true);
@@ -101,14 +84,12 @@ function getWeather(city) {
 			weatherDb.humidity = wData.humidity;
 			
 			//console.log(weatherDb);
-			
 			// Load the data to HTML structure
 			parseWeather(weatherDb);
 			
 		} else {
 			// We reached our target server, but it returned an error
 			console.log("Reached our target server: But, fell down");
-
 		}
 	};
 
@@ -118,19 +99,13 @@ function getWeather(city) {
 	};
 
 	request.send();
- 
 }
-
-
 
 // SET THE CITY TO A GLOBAL VARIABLE 
 function setCity(city) {
-	
 	gCity = city;
 	console.log("SETFUNC - " + gCity);
 }
-
-
 
 /* GET LOCATION from IP */ 
 function getLocation() {
@@ -139,21 +114,19 @@ function getLocation() {
 	request.open('GET', 'https://ipinfo.io/geo', true);
 	//request.open('GET', 'https://ipinfo.io/json', true);
 
-	console.log("LOCATION REQUEST" + request.status); 
+	console.log("LOCATION REQUEST > " + request.status); 
 	
 	request.onload = function() {
 		if (request.status >= 200 && request.status < 400) {
 			// Success!
 			var data = JSON.parse(request.responseText);
-			console.log(data.city, data.loc);
+			console.log("\nRAW CITY DATA >> " + data, data.city, data.loc);
 			
 			setCity(data.city, data.loc);
-			getWeather(data.city);
-			
+			getWeather(data.loc);
 		} else {
 			// We reached our target server, but it returned an error
 			console.log("Reached our target server: But, Error");
-
 		}
 	};
 
@@ -162,32 +135,26 @@ function getLocation() {
 		console.log("Connection Error, man!");
 	};
 
-	request.send();
-			
+	request.send();	
 }	
 	
 
 // GET HTML ELEMENT NODES
-window.onload = function () {
-	
+window.onload = function() {
+	console.log("START>>>\n");
 	tempL = document.getElementById("temperature");
-  iconL = document.getElementById("icon");
-  humidityL = document.getElementById("humidity");
+  	iconL = document.getElementById("icon");
+  	humidityL = document.getElementById("humidity");
 	locL = document.getElementById("location");
-  unitL = document.getElementById("unit");
-	
+  	unitL = document.getElementById("unit");
 
-	
-	
 	getLocation();
 	whatTime();
-	
 	getWeather();
 	
 	$('.card-wrapper').on('click', function () {
 		$('.card').toggleClass('flipped');
 		$('.card').removeClass('hover');
-
 		// Set Celsius / Fahrenheit values
 		isC = !isC;
 
@@ -197,12 +164,7 @@ window.onload = function () {
 			 document.getElementById('unit').innerHTML = "F";
 			 document.getElementById('tempF').innerHTML = weatherDb.tempF;
 		} 
-		 
 	});
-
-
-	
-	
 }
 
 
